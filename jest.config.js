f1e0c341dfd0d4567119e2f184e934893e1c2e40
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-04-12 09:14:27 -0700

    initial

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
 };
