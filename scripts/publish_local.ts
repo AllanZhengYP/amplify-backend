fd58aacd26cc9c41d0b8aa90b2b1efe53ce8df1a
MJ☔ <o.6180339+github@gmail.com>
2023-11-07 16:20:56 -0800

    exclude package.json and package-lock.json from dirty tree

--- scripts/publish_local.ts
+++ scripts/publish_local.ts
@@ -4,7 +4,9 @@ import * as path from 'path';
 
 const isCleanWorkingTree = async (): Promise<boolean> => {
   const buffer = await execa('git', ['status', '--porcelain']);
-  return !buffer.stdout.trim();
+  const getExcludedFiles = (line: string) =>
+    !line.includes('M package-lock.json') && !line.includes('M package.json');
+  return !buffer.stdout.trim().split('\n').filter(getExcludedFiles).length;
 };
 const isCleanTree = await isCleanWorkingTree();
 if (!isCleanTree) {
